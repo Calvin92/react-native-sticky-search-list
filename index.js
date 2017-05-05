df54587293f904077e2d3d6f0992d775a8247dce
Calvin92 <calvin1992.js@gmail.com>
2017-05-05 17:55:59 +0800

    Update: complete

--- index.js
+++ index.js
@@ -1,42 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react'
 import {
-  AppRegistry,
   StyleSheet,
-  KeyboardAvoidingView,
-  Text,
-  TextInput,
-  TouchableOpacity,
   View,
   ListView,
-  Dimensions,
+  Text,
+  TouchableOpacity,
   PanResponder,
-} from 'react-native';
+} from 'react-native'
 import _ from 'lodash'
 
-const data = [
-  {A: [1, 2, 3]},
-  {B: [1, 2, 3]},
-  {C: [1, 2, 3]},
-  {D: [1, 2, 3]},
-  {E: [1, 2, 3]},
-  {F: [1, 2, 3]},
-  {G: [1, 2, 3]},
-  {H: [1, 2, 3]},
-  {I: [1, 2, 3]},
-  {J: [1, 2, 3]},
-  {K: [1, 2, 3]},
-  {L: [1, 2, 3]},
-  {M: [1, 2, 3]},
-  {N: [1, 2, 3]},
-  {O: [1, 2, 3]},
-  {P: [1, 2, 3]},
-  {Q: [1, 2, 3]},
-  {R: [1, 2, 3]},
-  {S: [1, 2, 3]},
-]
-
 export default class StickySearchList extends Component {
-
   constructor(props) {
     super(props);
     this.panResponder = PanResponder.create({
@@ -56,7 +29,9 @@ export default class StickySearchList extends Component {
     const px = e.nativeEvent.locationX
 
     const index = Math.floor(py / 30)
-    if (px > 0 & px < 20) {
+
+    const { data, searchBarWidth = 20 } = this.props
+    if (px > 0 & px < searchBarWidth) {
       if (index < data.length) {
         this.listView.scrollTo({y: this[`offsetYFor${_.keys(data[index])[0]}`], animated: false})
       }
@@ -65,32 +40,29 @@ export default class StickySearchList extends Component {
 
   _renderRow (data, sectionID, rowID, highlightRow) {
     return (
-      <View style={{ paddingBottom: 80 }} onLayout={this._onCellLayout.bind(this, rowID)}>
-        <Text>{_.keys(data)[0]}</Text>
-        <View>
-          <Text>
-            佛山风景发啦发就看到了份
-            快乐的撒娇分开了多久啊看了份就是
-            打开房间啊但是看了积分卡收费集散地间发生的
-          </Text>
-        </View>
+      <View onLayout={this._onCellLayout.bind(this, rowID)}>
+        {this.props.renderRow(data, sectionID, rowID, highlightRow)}
       </View>
     )
   }
 
   _onCellLayout (index, event) {
-    // console.log(index)
+    const { data } = this.props
     this[`offsetYFor${_.keys(data[index])[0]}`] = event.nativeEvent.layout.y
-    // console.log(this[`offsetYFor${_.keys(data[index])[0]}`])
-    // console.log(event.nativeEvent.layout.y)
-    // console.log(_.keys(data[index])[0])
   }
 
   _handleSwitch (index) {
-    this.listView.scrollTo({y: this[`offsetYFor${_.keys(data[index])[0]}`], animated: true})
+    const { data } = this.props
+    this.listView.scrollTo({y: this[`offsetYFor${_.keys(data[index])[0]}`], animated: false})
   }
 
   render () {
+    const {
+      data,
+      searchBarBackgroundColor,
+      searchBarWidth,
+      searchBarTextStyle,
+    } = this.props
     return (
       <View style={styles.container}>
         <ListView
@@ -100,12 +72,16 @@ export default class StickySearchList extends Component {
          />
         <View
           {...this.panResponder.panHandlers}
-          style={{ width: 20, position: 'absolute', top: 50, right: 10, borderRadius: 5, backgroundColor: 'rgba(0, 0, 0, 0.6)' }}
+          style={[
+            styles.searchBar,
+            searchBarBackgroundColor ? { backgroundColor: searchBarBackgroundColor } : {},
+            searchBarWidth ? { width: searchBarWidth } : {}
+          ]}
         >
           {
             data.map((item, index) => (
-              <TouchableOpacity onPress={this._handleSwitch.bind(this, index)} key={index} style={{ height: 30, justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ fontSize: 18 }}>{_.keys(item)[0]}</Text>
+              <TouchableOpacity onPress={this._handleSwitch.bind(this, index)} key={index} style={styles.searchBarItem}>
+                <Text style={[{ fontSize: 18 }, searchBarTextStyle]}>{_.keys(item)[0]}</Text>
               </TouchableOpacity>)
             )
           }
@@ -115,6 +91,14 @@ export default class StickySearchList extends Component {
   }
 }
 
+StickySearchList.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object),
+  searchBarBackgroundColor: PropTypes.string,
+  searchBarWidth: PropTypes.number,
+  searchBarTextStyle: PropTypes.object,
+  renderRow: PropTypes.func.isRequired,
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -122,14 +106,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  searchBar: {
+    width: 20,
+    position: 'absolute',
+    top: 50,
+    right: 10,
+    borderRadius: 5,
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  searchBarItem: {
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+})
